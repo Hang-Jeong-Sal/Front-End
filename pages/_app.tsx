ad15a385dd1139cbbbdf5fed486175ae18e260ff
d0422 <rlfehd2013@naver.com>
2022-11-17 01:00:53 +0900

    Feat:모바일 뷰 적용 body:_app.tsx에 app.module.css를 적용하여 모바일 뷰로 화면 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import styles from '../styles/app.module.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <div className={styles.all}>
+        <Component {...pageProps} />
+      </div>
     </RecoilRoot>
   );
 }
