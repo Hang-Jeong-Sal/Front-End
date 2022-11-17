677341c317f3666a2405a2bed0f4888f6f17f99d
d0422 <rlfehd2013@naver.com>
2022-11-17 16:22:58 +0900

    Feat:공통컴포넌트(Menu)추가

--- components/common/Menu.tsx
+++ components/common/Menu.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
-
-const Menu = () => {
-  return <div>Menu</div>;
+import Image from 'next/image';
+import styled from 'styled-components';
+const Back = () => {
+  return (
+    <ImageContainer>
+      <Image src="/menu.svg" width={40} height={40} alt="이미지"></Image>
+    </ImageContainer>
+  );
 };
 
-export default Menu;
+const ImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export default Back;
