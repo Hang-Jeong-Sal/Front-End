c72f012e6fcce6207b55d0ad9ec1eb4b3185c4fd
d0422 <rlfehd2013@naver.com>
2022-11-17 16:09:22 +0900

    Feat:공용컴포넌트(Back) 추가

--- components/common/Back.tsx
+++ components/common/Back.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
-
+import Image from 'next/image';
+import styled from 'styled-components';
 const Back = () => {
-  return <div> &lt; </div>;
+  return (
+    <ImageContainer>
+      <Image src="/back.png" width={20} height={20} alt="이미지"></Image>
+    </ImageContainer>
+  );
 };
 
+const ImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 export default Back;
