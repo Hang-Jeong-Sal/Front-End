e3fb9f7886c0f80340c864a91d7602e0aff8184e
euije <exitfill1234@naver.com>
2022-11-17 20:43:01 +0900

    feat: library of upload file to AWS S3 추가

---
+++ lib/aws/s3.ts
@@ -0,0 +1,31 @@
+import AWS from "aws-sdk";
+
+type Region = 'ap-northeast-2';
+
+const AWSCognitoPoolId: string = process.env.NEXT_PUBLIC_AWS_COGNITO_POOL_ID!;
+const seoul: Region = "ap-northeast-2";
+const bucket: string = "hang-jeong-sal";
+const endpoint: string = "image/";
+
+const configUpdate = (region: Region, identityPoolId: string) => {
+    AWS.config.update({
+        region: region,
+        credentials: new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: identityPoolId,
+        }),
+    })
+};
+
+export const uploadFile = async (file: File) => {
+    configUpdate(seoul, AWSCognitoPoolId);
+    console.log(process.env);
+    const upload = new AWS.S3.ManagedUpload({
+        params: {
+            Bucket: bucket,
+            Key: endpoint + file.name,
+            Body: file,
+        },
+    });
+
+    return upload.promise();
+};
