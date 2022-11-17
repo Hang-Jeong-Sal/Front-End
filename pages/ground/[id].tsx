6ba97c5fbbf4c584c2497d83fbc988d449aaed7c
d0422 <rlfehd2013@naver.com>
2022-11-17 16:27:42 +0900

    feat: ground상세페이지 header 구성

--- pages/ground/[id].tsx
+++ pages/ground/[id].tsx
@@ -1,3 +1,15 @@
+import Back from '../../components/common/Back';
+import Menu from '../../components/common/Menu';
+import Header from '../../components/page/ground/[id]';
+import styles from '../../styles/ground.module.css';
+
 export default function Ground() {
-  return <></>;
+  return (
+    <>
+      <Header>
+        <Back />
+        <Menu />
+      </Header>
+    </>
+  );
 }
