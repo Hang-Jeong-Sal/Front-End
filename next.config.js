ab8a43a4aa10738c7373c72d8b934a6d1d7f2fe4
euije <exitfill1234@naver.com>
2022-11-17 20:41:22 +0900

    chore: hang-jeong-sal S3 리스트에추가

--- next.config.js
+++ next.config.js
@@ -3,9 +3,18 @@ const removeImports = require("next-remove-imports")();
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hang-jeong-sal.s3.ap-northeast-2.amazonaws.com",
+      },
+    ],
+  },
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig;
 module.exports = removeImports({
+  ...nextConfig,
   experimental: { esmExternals: true }
 });
