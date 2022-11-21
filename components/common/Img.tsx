e368d1b630fe705e67e7fd2165282212095b8aa9
d0422 <rlfehd2013@naver.com>
2022-11-22 06:46:18 +0900

    Feat(Img):이미지 컴포넌트 Wrapping하여 사용을 용이하게 개선함

---
+++ components/common/Img.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import styled from 'styled-components';
+interface IimageStyled {
+  width: number;
+  height: number;
+}
+interface Iimage extends IimageStyled {
+  src: string;
+}
+const Img = ({ width, height, src }: Iimage) => {
+  return (
+    <ImageContainer width={width} height={height}>
+      <Image src={src} alt={'이미지'} fill={true}></Image>
+    </ImageContainer>
+  );
+};
+const ImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: ${(props: IimageStyled) => props.width}vw;
+  height: ${(props: IimageStyled) => props.height}vh;
+`;
+export default Img;
