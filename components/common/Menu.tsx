c718ecaee78fd036ce78c582340bb01aeee089f4
d0422 <rlfehd2013@naver.com>
2022-11-22 07:33:41 +0900

    Refactor(Back,Menu,[id]):이미지 사이즈 조절, 메인 이미지 추가

--- components/common/Menu.tsx
+++ components/common/Menu.tsx
@@ -1,25 +1,20 @@
 import Image from 'next/image';
 import { Dispatch, SetStateAction } from 'react';
 import styled from 'styled-components';
+import Img from './Img';
 const Menu = ({
   setState,
 }: {
   setState: Dispatch<SetStateAction<boolean>>;
 }) => {
   return (
-    <ImageContainer
+    <div
       onClick={() => {
         setState((prev) => !prev);
       }}
     >
-      <Image src="/menu.svg" width={40} height={40} alt="이미지"></Image>
-    </ImageContainer>
+      <Img src="/menu.svg" width={4} height={4.5}></Img>
+    </div>
   );
 };
-
-const ImageContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
 export default Menu;
