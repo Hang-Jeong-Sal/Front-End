8bc017e8c935b5dbb3da55d14215e84ecfc14099
d0422 <rlfehd2013@naver.com>
2022-11-23 20:25:35 +0900

    Feat:전체 페이지 애니메이션 효과 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,26 @@ import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import styles from '../styles/app.module.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
+import { AnimatePresence, motion } from 'framer-motion';
 const queryClient = new QueryClient();
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <div className={styles.all}>
-          <Component {...pageProps} />
-        </div>
-      </RecoilRoot>
-    </QueryClientProvider>
+    <AnimatePresence>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <motion.div
+            transition={{ type: 'linear' }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            key={router.pathname}
+          >
+            <div className={styles.all}>
+              <Component {...pageProps} />
+            </div>
+          </motion.div>
+        </RecoilRoot>
+      </QueryClientProvider>
+    </AnimatePresence>
   );
 }
