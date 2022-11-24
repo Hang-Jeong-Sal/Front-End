f00f45aa93142ab3b0f2f8a78bfc362a3623cd9e
d0422 <rlfehd2013@naver.com>
2022-11-24 13:22:17 +0900

    Feat:빌려준 텃밭 페이지 추가

--- pages/mypage/borrowing.tsx
+++ pages/mypage/borrowing.tsx
@@ -1,13 +1,38 @@
+import { useQuery } from 'react-query';
+import { useRecoilState } from 'recoil';
+import { GroundData } from '..';
+import category, { ICategory } from '../../atoms/category';
 import Category from '../../components/page/mypage/Category';
 import { Header } from '../../components/page/mypage/Header';
+import { ItemMaker } from '../../components/page/mypage/ItemMaker';
+import {
+  getBorrowedTrading,
+  getBorrowedComplete,
+  getBorrowedEntire,
+} from '../../public/data/borrowed';
+import { useEffect } from 'react';
 
-const borrowing = () => {
+function getData(cate: ICategory) {
+  if (cate == '거래중') return getBorrowedTrading().then((res) => res.data);
+  else if (cate == '거래완료')
+    return getBorrowedComplete().then((res) => res.data);
+  else return getBorrowedEntire().then((res) => res.data);
+}
+const Borrowing = () => {
+  const [cate, setCate] = useRecoilState(category);
+  const { data } = useQuery<GroundData[]>(['likes', cate], () => getData(cate));
+  useEffect(() => {
+    setCate('전체');
+  }, []);
   return (
     <>
       <Header title={'빌린 텃밭'} />
-      <Category></Category>
+      <Category />
+      {data?.map((info, idx) => (
+        <ItemMaker props={info} key={idx} />
+      ))}
     </>
   );
 };
 
-export default borrowing;
+export default Borrowing;
