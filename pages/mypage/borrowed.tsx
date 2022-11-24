ca6efddb084bd9a0d5916d1e330d222681a3ac3d
d0422 <rlfehd2013@naver.com>
2022-11-24 14:39:34 +0900

    Refactor:기존 파일마다 있던 getData를 chooseAPI로 변경하고 import 하여 사용하도록 변경함

--- pages/mypage/borrowed.tsx
+++ pages/mypage/borrowed.tsx
@@ -4,19 +4,10 @@ import { useEffect } from 'react';
 import Category from '../../components/page/mypage/Category';
 import { Header } from '../../components/page/mypage/Header';
 import { ItemMaker } from '../../components/page/mypage/ItemMaker';
-
 import { GroundData } from '../../lib/interface/GroundData';
-import { ICategory } from '../../lib/type/category';
 import category from '../../atoms/category';
-import { getTrading } from '../../lib/api/getTrading';
-import { getComplete } from '../../lib/api/getComplete';
-import { getEntire } from '../../lib/api/getEntire';
+import { chooseAPI } from '../../lib/api/chooseApi';
 
-function getData(cate: ICategory) {
-  if (cate == '거래중') return getTrading().then((res) => res.data);
-  else if (cate == '거래완료') return getComplete().then((res) => res.data);
-  else return getEntire().then((res) => res.data);
-}
 export default function Borrowed() {
   const [cate, setCate] = useRecoilState(category);
 
@@ -24,7 +15,9 @@ export default function Borrowed() {
     setCate('전체');
   }, []);
 
-  const { data } = useQuery<GroundData[]>(['likes', cate], () => getData(cate));
+  const { data } = useQuery<GroundData[]>(['likes', cate], () =>
+    chooseAPI(cate)
+  );
   return (
     <>
       <Header title={'빌려준 텃밭'} />
--- pages/mypage/borrowing.tsx
+++ pages/mypage/borrowing.tsx
@@ -4,21 +4,15 @@ import Category from '../../components/page/mypage/Category';
 import { Header } from '../../components/page/mypage/Header';
 import { ItemMaker } from '../../components/page/mypage/ItemMaker';
 import { useEffect } from 'react';
-import { ICategory } from '../../lib/type/category';
 import category from '../../atoms/category';
 import { GroundData } from '../../lib/interface/GroundData';
-import { getTrading } from '../../lib/api/getTrading';
-import { getComplete } from '../../lib/api/getComplete';
-import { getEntire } from '../../lib/api/getEntire';
+import { chooseAPI } from '../../lib/api/chooseApi';
 
-function getData(cate: ICategory) {
-  if (cate == '거래중') return getTrading().then((res) => res.data);
-  else if (cate == '거래완료') return getComplete().then((res) => res.data);
-  else return getEntire().then((res) => res.data);
-}
 const Borrowing = () => {
   const [cate, setCate] = useRecoilState(category);
-  const { data } = useQuery<GroundData[]>(['likes', cate], () => getData(cate));
+  const { data } = useQuery<GroundData[]>(['likes', cate], () =>
+    chooseAPI(cate)
+  );
   useEffect(() => {
     setCate('전체');
   }, []);
