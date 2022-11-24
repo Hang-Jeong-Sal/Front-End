58a746c26bae733154fef8098647af56dcc6a3d9
euije <exitfill1234@naver.com>
2022-11-25 03:06:24 +0900

    feat: MUI theme Provider 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,23 @@ import { RecoilRoot } from 'recoil';
 import styles from '../styles/app.module.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { AnimatePresence, motion } from 'framer-motion';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { colors } from "../lib/color";
+import "../styles/scss/App.scss";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: colors.primary,
+    },
+    secondary: {
+      main: colors.secondary,
+    },
+    error: {
+      main: colors.error,
+    },
+  },
+});
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
@@ -17,9 +34,11 @@ export default function App({ Component, pageProps, router }: AppProps) {
             exit={{ opacity: 0 }}
             key={router.pathname}
           >
-            <div className={styles.all}>
-              <Component {...pageProps} />
-            </div>
+            <ThemeProvider theme={theme}>
+              <div className={styles.all}>
+                <Component {...pageProps} />
+              </div>
+            </ThemeProvider>
           </motion.div>
         </RecoilRoot>
       </QueryClientProvider>
