43d7373c41786f22da20a184851a510f32c68745
euije <exitfill1234@naver.com>
2022-11-26 16:00:44 +0900

    refactor: AppBar 컴포넌트화

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,15 @@ import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { theme, colors } from '../lib/color';
+import { ThemeProvider } from '@mui/material/styles';
+import { MainTheme, colors } from '../lib/color';
 
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import Fab from '@mui/material/Fab';
-import Button from '@mui/material/Button';
+
 import IconButton from '@mui/material/IconButton';
+import { Menu } from '@mui/material';
 import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
-import Select from '@mui/material/Select';
 import Modal from '@mui/material/Modal';
 
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
@@ -24,35 +22,11 @@ import ImportExportSharpIcon from '@mui/icons-material/ImportExportSharp';
 import AddIcon from '@mui/icons-material/Add';
 import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
 
-import { GroundData, GroundSearchOption, SortOption, sortOptions } from "../lib/interface/GroundData";
+import { GroundData, GroundSearchOption, sortOptions } from "../lib/interface/GroundData";
 
-import { VerticalContainer, VerticalScrollable, NavigationBar } from "../components/common/Interface";
+import { VerticalScrollable, NavigationBar, TwoRowAppBar } from "../components/common/Interface";
+import { SearchOptionButton } from "../components/common/MenuButtonList";
 import { GroundItem } from "../components/page/home";
-
-const AppBarTheme = createTheme({
-  palette: {
-    primary: {
-      main: colors.background,
-    },
-    secondary: {
-      main: colors.onBackground,
-    },
-    common: {
-      white: "#FFFFFF"
-    },
-  },
-});
-
-const ButtonTheme = createTheme({
-  palette: {
-    primary: {
-      main: colors.surface,
-    },
-    secondary: {
-      main: colors.onSurface,
-    },
-  },
-});
 
 export default function Home() {
   const [filterOptions, setFilterOptions] = useState<GroundSearchOption>({
@@ -68,8 +42,10 @@ export default function Home() {
     },
     sort: '최신 순'
   });
-  const [isModal, setIsModal] = useState<boolean>(false);
   const [groundData, setGroundData] = useState<GroundData[]>([]);
+
+  const [isModal, setIsModal] = useState<boolean>(false);
+  const [sortAnchorElement, setSortAnchorElement] = useState<null | HTMLElement>(null);
 
   const router = useRouter();
 
@@ -80,12 +56,17 @@ export default function Home() {
 
   return (
     <>
-      <ThemeProvider theme={theme}>
+      <Head>
+        <title>H.O.M.I</title>
+        <meta name="description" content="동네 근처의 텃밭을 빌려보세요. HOMI" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider theme={MainTheme}>
         <Fab size="medium" color={isModal ? "info" : "primary"} aria-label="add"
           onClick={() => { setIsModal(!isModal) }}
           sx={{
             position: "absolute",
-            bottom: "72px",
+            bottom: "64px",
             right: "16px",
             transition: {
               background: "1.2s"
@@ -102,12 +83,12 @@ export default function Home() {
                 width: "160px",
                 height: "40px",
                 position: "absolute",
-                bottom: "128px",
+                bottom: "116px",
                 right: "16px",
                 borderRadius: "15px",
                 backgroundColor: colors.background
               }}
-              onClick={() => {router.push(`/upload`)}}
+              onClick={() => { router.push(`/upload`) }}
             >
               <StorefrontOutlinedIcon style={{ marginRight: "12px" }} />
               {"내 땅 올리기"}
@@ -122,111 +103,89 @@ export default function Home() {
             }} />
         </Fab>
       </ThemeProvider>
-      <VerticalContainer style={{ position: "relative" }}>
-        <Head>
-          <title>H.O.M.I</title>
-          <meta name="description" content="동네 근처의 텃밭을 빌려보세요. HOMI" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        <ThemeProvider theme={AppBarTheme}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton
-                size="large"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={() => { }}
-                color="inherit"
-              >
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                  {"상도동"}
-                </Typography>
-                <ExpandMoreIcon />
-              </IconButton>
-              <div style={{ marginLeft: "auto" }}>
-                {
-                  [<SearchIcon key={"search"} />, <MenuIcon key={"menu"} />, <NotificationsOutlinedIcon key={"noti"} />].map((childMenu, idx) => {
-                    return (
-                      <IconButton
-                        key={idx}
-                        size="large"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        onClick={() => { }}
-                        color="inherit"
-                      >
-                        {childMenu}
-                      </IconButton>
-                    );
-                  })
-                }
-              </div>
-            </Toolbar>
-            <Toolbar>
-              <ThemeProvider theme={ButtonTheme}>
-                {
-                  ["유형", "면적", "위치", "가격"].map((searchOption, idx) => {
-                    return (
-                      <Button
-                        key={idx}
-                        variant="contained"
-                        disableElevation
-                        sx={{
-                          width: "60px",
-                          height: "30px",
-                          marginRight: "10px",
-                          boxShadow: "0px 1px 4px #DADADA",
-                          borderRadius: "20px"
-                        }}
-                      >
-                        <div style={{ whiteSpace: "nowrap" }}>{searchOption}</div>
-                        <ExpandMoreIcon />
-                      </Button>
-                    );
-                  })
-                }
-                <div style={{ marginLeft: "auto", marginRight: "8px", display: "flex", alignItems: "center" }}>
-                  <ImportExportSharpIcon />
-                  <Select
-                    value={filterOptions.sort}
-                    label="Age"
-                    onChange={(e) => { setFilterOptions({ ...filterOptions, sort: e.target.value as SortOption }) }}
-                    sx={{
-                      width: "30px",
-                      height: "30px",
-                      fontSize: "10px",
-                      '& .MuiSelect-icon': {
-                        display: "none"
-                      },
-                      '& .MuiOutlinedInput-input': {
-                        marginLeft: "-12px"
-                      }
-                    }}
+      <TwoRowAppBar>
+        <>
+          <IconButton
+            size="large"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={() => { }}
+            color="inherit"
+          >
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {"상도동"}
+            </Typography>
+            <ExpandMoreIcon />
+          </IconButton>
+          <div style={{ marginLeft: "auto" }}>
+            {
+              [<SearchIcon key={"search"} />, <MenuIcon key={"menu"} />, <NotificationsOutlinedIcon key={"noti"} />].map((childMenu, idx) => {
+                return (
+                  <IconButton
+                    key={idx}
+                    size="large"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={() => { }}
+                    color="inherit"
                   >
-                    {
-                      sortOptions.map((option, idx) => {
-                        return (
-                          <MenuItem key={idx} value={option}>{option}</MenuItem>
-                        );
-                      })
-                    }
-                  </Select>
-                </div>
-              </ThemeProvider>
-            </Toolbar>
-          </AppBar>
-        </ThemeProvider>
-        <VerticalScrollable>
+                    {childMenu}
+                  </IconButton>
+                );
+              })
+            }
+          </div>
+        </>
+        <>
           {
-            groundData?.map((ground, idx) => {
-              return <GroundItem props={ground} key={idx} />;
+            ["유형", "면적", "위치", "가격"].map((searchOption, idx) => {
+              return (
+                <SearchOptionButton
+                  key={idx}
+                  title={searchOption}
+                />
+              );
             })
           }
-        </VerticalScrollable >
-
-        <NavigationBar currentFeature={"home"} style={{ marginTop: "auto" }} />
-      </VerticalContainer >
+          <div
+            style={{ marginLeft: "auto", marginRight: "8px", display: "flex", alignItems: "center" }}
+            onClick={(e) => { setSortAnchorElement(sortAnchorElement === null ? e.currentTarget : null) }}
+          >
+            <ImportExportSharpIcon />
+            <div style={{ fontWeight: "500", fontSize: "12px", whiteSpace: "nowrap" }}>
+              {filterOptions.sort}
+            </div>
+            <Menu
+              open={sortAnchorElement !== null}
+              // onClose={() => {setSortAnchorElement(null)}}
+              anchorEl={sortAnchorElement}>
+              {
+                sortOptions.map((option, idx) => {
+                  return (
+                    <MenuItem
+                      key={idx} value={option}
+                      onClick={() => {
+                        setFilterOptions((prev) => prev = { ...filterOptions, sort: option });
+                        setSortAnchorElement(null);
+                      }}
+                    >
+                      {option}
+                    </MenuItem>
+                  );
+                })
+              }
+            </Menu>
+          </div>
+        </>
+      </TwoRowAppBar>
+      <VerticalScrollable style={{ height: `calc(100vh - ${112 + 48}px)` }}>
+        {
+          groundData?.map((ground, idx: number) => {
+            return <GroundItem props={ground} key={idx} />;
+          })
+        }
+      </VerticalScrollable >
+      <NavigationBar currentFeature={"home"} style={{ marginTop: "auto", position: "absolute" }} />
     </>
   );
 }
