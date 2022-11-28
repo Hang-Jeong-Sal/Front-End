e6eab0954780e6cbcccb7d9191b8e06c61f754e3
d0422 <rlfehd2013@naver.com>
2022-11-28 13:58:06 +0900

    refactor:타입 형태 백엔드와 동일하게 변경

--- lib/interface/GroundData.ts
+++ lib/interface/GroundData.ts
@@ -1,95 +1,101 @@
-import { GroundCategory } from "../type/ground";
-import { GroundConvenient } from "../type/ground";
+import { GroundCategory } from '../type/ground';
+import { GroundConvenient } from '../type/ground';
 import { Dayjs } from 'dayjs';
 import dayjs from 'dayjs';
 
 export interface GroundData {
-  id: number
-  title: string
-  address: string
-  imgUrl: string[]
-  price: number
-  address_1: string
-  address_2: string
-  address_3: string
-  area: number
-  likeCount: number
-  categories: GroundCategory[]
+  id: number;
+  title: string;
+  address: string;
+  imgUrl: string[];
+  price: number;
+  address_1: string;
+  address_2: string;
+  address_3: string;
+  area: number;
+  likeCount: number;
+  categories: GroundCategory[];
 }
 
 export interface GroundDetailData {
-  userId: number,
-  name: string,
-  image?: string[],
-  category?: string[],
-  convenient?: GroundConvenient[],
-  area: number,
-  period: {
-    start?: Dayjs | null,
-    end?: Dayjs | null
-  },
-  price: number,
-  address: string,
+  userId: number;
+  name: string;
+  address: string;
+  area: number;
+  price: number;
+  image?: string[];
+  renderStartDate?: string;
+  renderFinishDate?: string;
+  introduction: string;
   location: {
-    x: number,
-    y: number
-  }
-  introduction: string,
+    x: number;
+    y: number;
+  };
+  // category?: string[];
+  // convenient?: GroundConvenient[];
 }
 
-
-export type SearchOption = 'category' | 'area' | 'radius' | 'price' | 'convenient' | 'period' | 'sort';
-export const SearchOption_ko: {[K in SearchOption]: string} = {
-  'category': "유형",
-  'area': "범위",
-  'radius': "지역",
-  'price': "가격",
-  'convenient': "부대 시설",
-  'period': "대여 기간",
-  'sort': "정렬"
-}
+export type SearchOption =
+  | 'category'
+  | 'area'
+  | 'radius'
+  | 'price'
+  | 'convenient'
+  | 'period'
+  | 'sort';
+export const SearchOption_ko: { [K in SearchOption]: string } = {
+  category: '유형',
+  area: '범위',
+  radius: '지역',
+  price: '가격',
+  convenient: '부대 시설',
+  period: '대여 기간',
+  sort: '정렬',
+};
 
 export type SortOption = 'ascending' | 'descending' | 'popular';
-export const SortOption_ko: {[K in SortOption]: string} = {
-  'ascending': "최신 순",
-  'descending': "오래된 순",
-  'popular': "인기 많은 순"
+export const SortOption_ko: { [K in SortOption]: string } = {
+  ascending: '최신 순',
+  descending: '오래된 순',
+  popular: '인기 많은 순',
 };
 
 export interface GroundSearchOption {
-  category: GroundCategory[],
-  area: { // m^2
-    from: number,
-    to: number
-  },
-  radius: number, // km
-  price: { // won
-    from: number,
-    to: number
-  },
-  convenient: GroundConvenient[],
+  category: GroundCategory[];
+  area: {
+    // m^2
+    from: number;
+    to: number;
+  };
+  radius: number; // km
+  price: {
+    // won
+    from: number;
+    to: number;
+  };
+  convenient: GroundConvenient[];
   period: {
-    from: Dayjs,
-    to: Dayjs
-  },
-  sort: SortOption,
+    from: Dayjs;
+    to: Dayjs;
+  };
+  sort: SortOption;
 }
 
 export const DefaultGroundSearchOption: GroundSearchOption = {
   category: [],
   area: {
     from: 10,
-    to: 15000
+    to: 15000,
   },
   radius: 10,
   price: {
     from: 1000,
-    to: 150000
+    to: 150000,
   },
   convenient: [],
   period: {
     from: dayjs().subtract(2, 'month'),
-    to: dayjs()
+    to: dayjs(),
   },
-  sort: 'ascending'
+  sort: 'ascending',
 };
