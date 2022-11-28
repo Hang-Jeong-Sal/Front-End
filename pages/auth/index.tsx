cfba2cb5ad7d4b4c0f29b5a7bf9e9d5859839cb4
euije <exitfill1234@naver.com>
2022-11-28 21:55:15 +0900

    style: Make static resources abstraction file

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -8,8 +8,7 @@ import { MainTheme } from '../../lib/color';
 
 import { VerticalContainer, HorizontalContentContainer } from '../../components/common/Interface';
 
-export const LogoUrl = "https://hang-jeong-sal.s3.ap-northeast-2.amazonaws.com/image/icon.png";
-export const MockupUrl = "https://hang-jeong-sal.s3.ap-northeast-2.amazonaws.com/image/mockup.png";
+import { LogoUrl, MockupUrl } from '../../lib/resource';
 
 const Auth = () => {
   const router = useRouter();
