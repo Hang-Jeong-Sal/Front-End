401560a8efc60f6db0d2eb9863e63ea8a0d0d742
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-11-30 13:45:59 +0900

    Style(marker): marker styling

--- lib/api/getEntire.ts
+++ lib/api/getEntire.ts
@@ -6,38 +6,6 @@ export async function getEntire(): Promise<Ires> {
       const result = {
         data: [
           {
-<<<<<<< HEAD
-            name: '민지네 텃밭',
-            address: '서울특별시 동작구',
-            price: 10000,
-            like_count: 3,
-            image:
-              'https://user-images.githubusercontent.com/12531340/203138488-ab6862b0-7ac8-4092-b154-9466726eab45.png',
-          },
-          {
-            name: '중앙농장',
-            address: '서울특별시 동작구',
-            price: 10000,
-            like_count: 15,
-            image:
-              'https://user-images.githubusercontent.com/12531340/203138498-ad33f399-9f58-4267-99ab-b080e80b643a.png',
-          },
-          {
-            name: '의제네 텃밭',
-            address: '서울특별시 동작구',
-            price: 10000,
-            like_count: 3,
-            image:
-              'https://user-images.githubusercontent.com/12531340/203138488-ab6862b0-7ac8-4092-b154-9466726eab45.png',
-          },
-          {
-            name: '서울농장',
-            address: '부산광역시 중구',
-            price: 10000,
-            like_count: 15,
-            image:
-              'https://user-images.githubusercontent.com/12531340/203138498-ad33f399-9f58-4267-99ab-b080e80b643a.png',
-=======
             title: '민지네 텃밭',
             address: '서울특별시 동작구',
             price: 10000,
@@ -68,7 +36,6 @@ export async function getEntire(): Promise<Ires> {
             likeCount: 15,
             imgUrl:
               ['https://user-images.githubusercontent.com/12531340/203138498-ad33f399-9f58-4267-99ab-b080e80b643a.png'],
->>>>>>> 5f219e73c19bdf44dfbd63ac40f01463c67aa07d
           },
         ],
       } as Ires;
